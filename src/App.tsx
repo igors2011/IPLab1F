029d5e4b669ee9a93d3edc754de7a2b633bce2bb
Мк Игорь <igors-2011@mail.ru>
2023-05-01 18:28:44 +0400

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from './Counter';
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. Hello world
+          <Counter start={10} step={5} />
         </p>
         <a
           className="App-link"
