fe42eb7b965071d37d937aa7d9f98780f2e8c8a4
Мк Игорь <igors-2011@mail.ru>
2023-05-01 18:28:26 +0400

    full lab

---
+++ src/Button.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+export {};
+
+interface ButtonProps {
+    text: string;
+    onClick: () => any;
+}
+
+var Button = (props: ButtonProps) => {
+    return (
+        <button onClick={() => props.onClick}>{props.text}</button>
+    );
+};
+export default Button;
